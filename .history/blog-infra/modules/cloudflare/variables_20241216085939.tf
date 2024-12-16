7855898023ad3674b46624b386d1cedda5e96098
Diego Zarpelon <zarpelon@icloud.com>
2024-12-16 10:12:42 +0100

    removed additional domain name from terraform.tfvars

---
+++ .history/blog-infra/modules/cloudflare/variables_20241216085939.tf
@@ -0,0 +1,15 @@
+variable "hcp_client_id" {
+  description = "The client ID for HCP"
+  type        = string
+}
+
+variable "hcp_client_secret" {
+  description = "The client secret for HCP"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The domain name managed by Cloudflare."
+  type        = string
+}
+
