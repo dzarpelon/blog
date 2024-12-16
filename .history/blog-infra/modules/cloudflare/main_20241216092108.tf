e72c50a86edf7cd8e08a6e9018ed5784a7b82cc6
Diego Zarpelon <zarpelon@icloud.com>
2024-12-16 10:12:42 +0100

    removed additional domain name from terraform.tfvars

---
+++ .history/blog-infra/modules/cloudflare/main_20241216092108.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "~>0.100"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "~>4.48"
+    }
+     aws = {
+      source = "hashicorp/aws"
+      version = "~>5.81"
+    }
+  }
+}
+## Fetch Secrets from HCP Vault
+data "hcp_vault_secrets_app" "cloudflare" {
+  app_name   = "Cloudflare"
+}
+
+# set aws region for ACM certificate generation for Cloudfront:
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"  # ACM certificates for CloudFront must be in us-east-1
+}
+#Cloudflare provider block
+
+# Set Cloudflare token
+provider "cloudflare" {
+  api_token = data.hcp_vault_secrets_app.cloudflare.secrets["zone_edit"]
+}
+
+# Query the Cloudflare zone dynamically using the domain name
+data "cloudflare_zone" "blog_zone" {
+  name = var.domain_name
+}
+
+
+# Create a Cloudflare DNS record for the Cloudfront 
+resource "cloudflare_record" "blog" {
+  zone_id = data.cloudflare_zone.blog_zone.id
+  name    = "blog"
+  content = "192.0.2.2"
+  type    = "A"
+  ttl     = 3600
+ }
+
+# Loop through validation details and create DNS records
+resource "cloudflare_record" "acm_validation" {
+  for_each = { for v in var.validation_details : v.resource_record_name => v }
+
+  zone_id = data.cloudflare_zone.blog_zone.id
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  content   = each.value.resource_record_value
+  ttl     = 300
+}
---
+++ .history/blog-infra/modules/cloudflare/variables_20241216090136.tf
@@ -0,0 +1,26 @@
+variable "hcp_client_id" {
+  description = "The client ID for HCP"
+  type        = string
+}
+
+variable "hcp_client_secret" {
+  description = "The client secret for HCP"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The domain name managed by Cloudflare."
+  type        = string
+}
+
+# Variable for storing ACM validation details for DNS record creation.
+# This includes domain name, record name, record type, and record value.
+variable "validation_details" {
+  description = "ACM validation details for creating DNS records."
+  type = list(object({
+    domain_name           = string
+    resource_record_name  = string
+    resource_record_type  = string
+    resource_record_value = string
+  }))
+}
